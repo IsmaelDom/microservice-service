070c9745b4732fc0967b245a5cf64ae7b0210789
Ismael Domínguez Martínez <ismaeldom11@gmail.com>
2022-10-27 11:23:57 -0500

    Inicio del proyecto.

---
+++ src/main/java/com/demo/microserviceservice/MicroserviceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.microserviceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/microserviceservice/MicroserviceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.microserviceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
